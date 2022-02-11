60cc74ce26d624a97049fd2cd4821109c94ed643
Sergii Ostraczerta <skylitik@outlook.com>
2022-02-11 19:28:07 +0100

    Change the decimal in result.Its show 0.00 zero at the end

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -3,9 +3,11 @@
 print("Welcome to the tip calculator")
 summary = input("What was the total bill? $")
 summary_float = float(summary)
-percent = input("What percentage tip would you like to give? 10, 12, or 15? ")
-people = input("How many people to split the bill? ")
-percent_summary = ((int(percent) / 100) * summary_float)
-total_with_tip = (summary_float + percent_summary) / int(people)
-result = round(total_with_tip,2)
+percent = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+percent_summary = ((percent / 100) * summary_float)
+total_with_tip = summary_float + percent_summary
+bil_per_person = total_with_tip / people
+result = round(bil_per_person,2)
+result = "{:.2f}".format(bil_per_person)
 print(f"Each person should pay: ${result}")
