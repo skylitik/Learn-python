8ea0d310857505e599fbfa37e8bb7b00fdf135fe
Sergii Ostraczerta <skylitik@outlook.com>
2022-02-12 18:54:53 +0100

    remove useless if

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -7,11 +7,11 @@ bmi = weight / (height ** 2)
 bmi_as_int = round(bmi)
 if bmi_as_int < 18.5:
     print(f"Your BMI is {bmi_as_int}, you are underweight.")
-elif 25 > bmi_as_int >= 18.5:
+elif 25 > bmi_as_int:
     print(f"Your BMI is {bmi_as_int}, you have a normal weight.")
-elif 30 > bmi_as_int >= 25:
+elif 30 > bmi_as_int:
     print(f"Your BMI is {bmi_as_int}, you are slightly overweight.")
-elif 35 > bmi_as_int >= 30:
+elif 35 > bmi_as_int:
     print(f"Your BMI is {bmi_as_int}, you are obese.")
 else:
     print(f"You BMI is {bmi_as_int}, you are clinically obese.")
