c088333be096930418711b069a8694308393d1bf
Las <skylitik@gmail.com>
2023-08-08 16:39:09 +0200

    edit import, delete first print line

--- day4_2_las.py
+++ day4_2_las.py
@@ -1,13 +1,14 @@
+import random
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-import random
-print(names)
+
+
 
 names_len = len(names)
 names_rand = random.randint(0, names_len)
-chart = names[names_rand]
+chart = str(names[names_rand])
 print(f"{chart} is going to buy the meal today!")
