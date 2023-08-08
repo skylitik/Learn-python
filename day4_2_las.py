19c6d1600abed262a0a58af64efc2efaaa56da9d
Las <skylitik@gmail.com>
2023-08-08 17:01:45 +0200

    Edit code with choice method

--- day4_2_las.py
+++ day4_2_las.py
@@ -2,13 +2,10 @@ import random
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
-
-
-
 names_len = len(names)
 names_rand = random.randint(0, names_len -1)
 chart = names[names_rand]
 print(f"{chart} is going to buy the meal today!")
+
+#chart_pay = random.choice(names)
+#print(chart_pay)
